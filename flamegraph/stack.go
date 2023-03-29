221f408b0ccadb5521ea8dfc1b47f4345ebe0cb1
qsliu <qsliu2017@outlook.com>
2023-03-29 13:55:17 +0000

    flamegraph/stack

---
+++ flamegraph/stack.go
@@ -0,0 +1,42 @@
+package flamegraph
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+type Proc struct {
+	Name     string  `json:"name"`
+	Value    int     `json:"value"`
+	Children []*Proc `json:"children"`
+}
+
+type Stack []*Proc
+
+func NewStack() Stack {
+	return make(Stack, 1)
+}
+
+func (s *Stack) Push(name string, start int) {
+	p := &Proc{
+		Name:     name,
+		Value:    start,
+		Children: make([]*Proc, 0),
+	}
+	last := (*s)[len(*s)-1]
+	last.Children = append(last.Children, p)
+	*s = append(*s, p)
+}
+
+func (s *Stack) Pop(name string, end int) {
+	last := (*s)[len(*s)-1]
+	last.Value = end - last.Value
+	*s = (*s)[:len(*s)-1]
+}
+
+func (s *Stack) Json() string {
+	var buf bytes.Buffer
+	encoder := json.NewEncoder(&buf)
+	encoder.Encode((*s)[0])
+	return buf.String()
+}
