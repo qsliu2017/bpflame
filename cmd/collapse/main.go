096e52a450b5919460d0dcd75f7134e5905cfaed
qsliu <qsliu2017@outlook.com>
2023-04-04 08:40:11 +0000

    collapse flag `--ignore` & `--ignore-time`

--- cmd/collapse/main.go
+++ cmd/collapse/main.go
@@ -15,13 +15,18 @@ import (
 var (
 	input  string
 	output string
-	pid    uint64
+
+	pid        uint64
+	ignore     string
+	ignoreTime bool
 )
 
 func init() {
 	flag.StringVar(&input, "input", "", "")
 	flag.StringVar(&output, "output", "", "")
 	flag.Uint64Var(&pid, "pid", 0, "")
+	flag.StringVar(&ignore, "ignore", "", "")
+	flag.BoolVar(&ignoreTime, "ignore-time", false, "")
 	flag.Parse()
 }
 
@@ -51,6 +56,11 @@ func main() {
 		out = f
 	}
 
+	ignoreFunc := map[string]struct{}{}
+	for _, f := range strings.Split(ignore, ",") {
+		ignoreFunc[f] = struct{}{}
+	}
+
 	stack := []*bpf.Event{}
 	for {
 		var e bpf.Event
@@ -63,6 +73,7 @@ func main() {
 	}
 	sort.Slice(stack, func(i, j int) bool { return stack[i].Ts < stack[j].Ts })
 
+	var ignoreLevel int
 	probeStack := []string{}
 	tsStack := []uint64{}
 	for _, e := range stack {
@@ -71,11 +82,22 @@ func main() {
 				logger.Error("probe stack is not consistent", zap.String("probe", e.Probe))
 				continue
 			}
-			start := tsStack[len(tsStack)-1]
-			fmt.Fprintf(out, "%s %d\n", strings.Join(probeStack, ";"), e.Ts-start)
+			if ignoreLevel == 0 {
+				duration := e.Ts - tsStack[len(tsStack)-1]
+				if ignoreTime {
+					duration = 1
+				}
+				fmt.Fprintf(out, "%s %d\n", strings.Join(probeStack, ";"), duration)
+			}
+			if _, ignore := ignoreFunc[e.Probe]; ignore {
+				ignoreLevel--
+			}
 			probeStack = probeStack[:len(probeStack)-1]
 			tsStack = tsStack[:len(tsStack)-1]
 		} else {
+			if _, ignore := ignoreFunc[e.Probe]; ignore {
+				ignoreLevel++
+			}
 			probeStack = append(probeStack, e.Probe)
 			tsStack = append(tsStack, e.Ts)
 		}
